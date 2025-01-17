b0355176dd2c4f716e261832a03d3fd3819c50fc
hamdy-farg <farghamdy72@gmail.com>
2025-01-17 06:51:36 +0200

    don

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,3 @@
-export default async function loadBalancer(chinaDownload, USDownload) {
-  return await Promise.race([chinaDownload, USDownload]).then((res) => res);
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
 }
